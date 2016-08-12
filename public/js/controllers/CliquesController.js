4e50b1a58d7ba8a1da9a44f4733d39906b9aae29
Neto Deolino <netodeolino@gmail.com>
2016-08-12 14:09:38 -0300

    Versão 0.0.2

--- public/js/controllers/CliquesController.js
+++ public/js/controllers/CliquesController.js
@@ -1,1 +1,10 @@
 /* Configure all clicks and buttons here */
+
+function redirectPreviusView (params) {
+	var posicao = params;
+	// Continue...
+}
+
+function redirectNextView (params) {
+	// Continue...
+}
--- public/js/controllers/UsuarioController.js
+++ public/js/controllers/UsuarioController.js
@@ -1,1 +1,10 @@
 /* All actions of the user */
+
+function addNewUser (params) {
+	var id = params.id_user;
+	// Continue...
+}
+
+function remUser (params) {
+	// Continue...
+}
